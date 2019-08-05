c4e1c6355f22383e8e7ac59fc6f4ed78b60195f6
Anderson Mendes <anderson2000ano@gmail.com>
2019-08-05 17:45:35 -0300

    setup the routes

--- index.php
+++ index.php
@@ -6,9 +6,58 @@ require './vendor/autoload.php';
 
 $app = new \Slim\App;
 
- $app->get('/', function (Request $request, Response $response) use ($app) {
-    $response->getBody()->write("yay");
-    return $response;
+/**
+ * get all books
+ */
+$app->get('/book', function (Request $request, Response $response) use ($app) {
+    $return = $response->withJson(['msg' => 'books list'], 200);
+    return $return;
 });
+
+/**
+ * get a specific book
+ */
+$app->get('/book/{id}', function (Request $request, Response $response) use ($app) {
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');    
+    
+    $return = $response->withJson(['msg' => "Details about book {$id}"], 200)
+        ->withHeader('Content-type', 'application/json');
+    return $return;
+});
+
+/**
+ * create a new book
+ */
+$app->post('/book', function (Request $request, Response $response) use ($app) {
+    $return = $response->withJson(['msg' => "Creating a new book"], 201)
+        ->withHeader('Content-type', 'application/json');
+    return $return;
+});
+
+/**
+ * update a book
+ */
+$app->put('/book/{id}', function (Request $request, Response $response) use ($app) {
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');    
+
+    $return = $response->withJson(['msg' => "Updating the book {$id}"], 200)
+        ->withHeader('Content-type', 'application/json');
+    return $return;
+});
+
+/**
+ * delete a book
+ */
+$app->delete('/book/{id}', function (Request $request, Response $response) use ($app) {
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');    
+    
+    $return = $response->withJson(['msg' => "Deleting the book {$id}"], 204)
+        ->withHeader('Content-type', 'application/json');
+    return $return;
+});
+
 
 $app->run();
